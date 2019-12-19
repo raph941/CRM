f62a06be433596f643149b8774176c400f75f953
raph941 <raph941@gmail.com>
2019-12-19 16:14:46 +0100

    updated STATIC_ROOT

--- CRM/settings.py
+++ CRM/settings.py
@@ -144,7 +144,9 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
